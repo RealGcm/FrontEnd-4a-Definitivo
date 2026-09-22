7101c9b182d90287f3eb53ae0cf5d618ea162a70
RealGcm <272425@unifio.edu.br>
2026-09-22 14:29:24 +0000

    fim

--- Atividade/src/app/app-routing-module.ts
+++ Atividade/src/app/app-routing-module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'lista1', loadChildren: () => import('./lista1/lista1-module').then(m => m.Lista1Module) }
+  { path: 'lista1', loadChildren: () => import('./lista1/lista1-module').then(m => m.Lista1Module) },
+  { path: 'lista2', loadChildren: () => import('./lista2/lista2-module').then(m => m.Lista2Module) }
 ];
 
 @NgModule({
